23ef0683132d7313aaad4f9c69d4a08cdf3d45a7
urly3 <u@rl.yyy>
2024-04-27 14:04:29 +0100

    start on instrs

--- src/chip8.zig
+++ src/chip8.zig
@@ -1,34 +1,101 @@
 const std = @import("std");
 const CPU = struct {
-    a: u8 = 0,
-    b: u8 = 0,
-    c: u8 = 0,
-    d: u8 = 0,
-    e: u8 = 0,
-    f: u8 = 0,
-    g: u8 = 0,
-    h: u8 = 0,
-    i: u8 = 0,
-    j: u8 = 0,
-    k: u8 = 0,
-    l: u8 = 0,
-    m: u8 = 0,
-    n: u8 = 0,
-    o: u8 = 0,
-    p: u8 = 0,
-    memory: []u8,
+    // 16 8bit registers - 0x00 thorugh 0x0f.
+    // index into them to get values.
+    var registers: [16]u8 = .{0} ** 10;
+    var memory: [4096]u8 = undefined;
 };
 
 fn readInstruction(cpu: *CPU) void {
     const cur: [*]u8 = cpu.memory.ptr;
 
-    if (cur == 0) {
+    if (cur & 0x00 < 0x10) {
         switch (cur[1]) {
+            // clear.
             0xe0 => return,
+            // return from sr.
             0xee => return,
+            // execute sr.
             else => return,
         }
 
+        return;
+    }
+
+    if (cur & 0x1000 == 0x1000) {
+        // jump to addr.
+        return;
+    }
+
+    if (cur & 0x2000 == 0x3000) {
+        // execute sr.
+        return;
+    }
+    if (cur & 0x3000 == 0x3000) {
+        // skip next if rx == nn.
+        return;
+    }
+    if (cur & 0x4000 == 0x4000) {
+        // skip next if rx != nn.
+        return;
+    }
+    if (cur & 0x5000 == 0x5000) {
+        // skip next if rx == ry.
+        return;
+    }
+    if (cur & 0x6000 == 0x6000) {
+        // store nn in rx;
+        return;
+    }
+    if (cur & 0x7000 == 0x7000) {
+        // add nn to rx.
+        return;
+    }
+    if (cur & 0x8000 == 0x8000) {
+        // store ry in rx.
+
+        // set rx to rx | ry.
+
+        // set rx to rx & ry.
+
+        // set rx to rx ^ ry.
+
+        // add ry to rx,
+        // set rf to carry.
+
+        // sub ry from rx,
+        // set rf to !borrow.
+
+        // store ry >> in rx,
+        // set rf to ry to lsb pre-shift.
+
+        // store ry - rx in rx,
+        // set rf to !borrow.
+
+        // store ry << in rx,
+        // set rf to msb pre-shift.
+        return;
+    }
+    if (cur & 0x9000 == 0x9000) {
+        // skip next if rx != ry.
+        return;
+    }
+    if (cur & 0xa000 == 0xa000) {
+        return;
+    }
+    if (cur & 0xb000 == 0xb000) {
+        return;
+    }
+    if (cur & 0xc000 == 0xc000) {
+        return;
+    }
+    if (cur & 0xd000 == 0xd000) {
+        return;
+    }
+    if (cur & 0xe000 == 0xe000) {
+        return;
+    }
+    if (cur & 0xf000 == 0xf000) {
         return;
     }
 }
