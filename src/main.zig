381ff5fca6e7e88f3c8cd7f96fea4ea36672d6ba
urly3 <u@rl.yyy>
2026-05-29 17:51:49 +0100

    added: rom file arg + snapshot
    take filepath for rom to load into the chip8.
    
    a Snapshot is an immutable copy of the current state of the provided
    chip8.
    
    will be good for future additions / debugging (I think).
    
    +:
    some general cleanup

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,61 @@
 const std = @import("std");
 
 const debug = false;
+const cli = true;
+
 const screen_width = 64;
 const screen_height = 32;
+
 var random: std.Random = undefined;
+
+const Snapshot = struct {
+    pc: u16,
+    opcode: u16,
+    x: u4,
+    y: u4,
+    n: u4,
+    nn: u8,
+    nnn: u12,
+    r: [16]u8,
+    vx: u8,
+    vy: u8,
+    vf: u8,
+    i: u12,
+
+    fn create(chip8: *Chip8) Snapshot {
+        const pc = chip8.program_counter;
+        const opcode_bytes = chip8.memory[pc .. pc + 2];
+
+        const opcode = std.mem.readVarInt(u16, opcode_bytes, .big);
+        const x: u4 = @truncate(opcode >> 8);
+        const y: u4 = @truncate(opcode >> 4);
+        const n: u4 = @truncate(opcode);
+        const nn: u8 = @truncate(opcode);
+        const nnn: u12 = @truncate(opcode);
+
+        const r = chip8.registers;
+        const vx = r[x];
+        const vy = r[y];
+        const vf = r[0xf];
+
+        const i = chip8.address_register;
+
+        return .{
+            .pc = pc,
+            .opcode = opcode,
+            .r = r,
+            .vx = vx,
+            .vy = vy,
+            .vf = vf,
+            .i = i,
+            .x = x,
+            .y = y,
+            .n = n,
+            .nn = nn,
+            .nnn = nnn,
+        };
+    }
+};
 
 const Rom = struct {
     data: []const u8,
@@ -14,6 +66,10 @@ const Rom = struct {
         return .{
             .data = try reader.interface.allocRemaining(allocator, .unlimited),
         };
+    }
+
+    fn unload(rom: *const Rom, allocator: std.mem.Allocator) void {
+        allocator.free(rom.data);
     }
 };
 
@@ -28,7 +84,6 @@ const Chip8 = struct {
     program_counter: u16 = 0x200,
     stack_index: u16 = 0,
 
-    // load the given rom into memory
     fn loadRom(chip8: *Chip8, rom: *const Rom) void {
         const end = 0x200 + rom.data.len;
         @memcpy(chip8.memory[0x200..end], rom.data);
@@ -39,20 +94,21 @@ const Chip8 = struct {
     }
 
     // step through the next instruction in memory
-    // at program_counter offset
-    //
-    // all instructions are 2 bytes long
+    // at offset held in program_counter
     fn step(chip8: *Chip8) void {
-        const pc = &chip8.program_counter;
-        const opcode_bytes = chip8.memory[pc.* .. pc.* + 2];
-        const opcode_uint = std.mem.readVarInt(u16, opcode_bytes, .big);
+        const memory: []u8 = &chip8.memory;
+        const display: []u8 = memory[0xf00..];
 
-        // nxym
-        const x: u4 = @truncate(opcode_uint >> 8);
-        const y: u4 = @truncate(opcode_uint >> 4);
-        const n: u4 = @truncate(opcode_uint);
-        const nn: u8 = @truncate(opcode_uint);
-        const nnn: u12 = @truncate(opcode_uint);
+        const pc = &chip8.program_counter;
+
+        const opcode_bytes = chip8.memory[pc.* .. pc.* + 2];
+        const opcode = std.mem.readVarInt(u16, opcode_bytes, .big);
+
+        const x: u4 = @truncate(opcode >> 8);
+        const y: u4 = @truncate(opcode >> 4);
+        const n: u4 = @truncate(opcode);
+        const nn: u8 = @truncate(opcode);
+        const nnn: u12 = @truncate(opcode);
 
         const vx = &chip8.registers[x];
         const vy = &chip8.registers[y];
@@ -60,15 +116,12 @@ const Chip8 = struct {
 
         const i = &chip8.address_register;
 
-        const display: []u8 = chip8.memory[0xf00..];
-
         if (debug) {
-            std.debug.print("display starts at address: {p}", .{&display[0]});
+            std.debug.print("display starts at address: {p}\n", .{&display[0]});
 
             std.debug.print("pc: {x}\n", .{pc.*});
 
-            std.debug.print("op bytes: {any}\n", .{opcode_bytes});
-            std.debug.print("op uint: {x}\n", .{opcode_uint});
+            std.debug.print("op uint: {x}\n", .{opcode});
 
             std.debug.print("x: {x}\n", .{x});
             std.debug.print("y: {x}\n", .{y});
@@ -76,24 +129,23 @@ const Chip8 = struct {
             std.debug.print("nn: {x}\n", .{nn});
             std.debug.print("nnn: {x}\n", .{nnn});
 
-            std.debug.print("vx: {x}\n", .{vx.*});
-            std.debug.print("vy: {x}\n", .{vy.*});
-            std.debug.print("vf: {x}\n", .{vf.*});
+            for (0..16) |idx| {
+                std.debug.print("v{d}: {x}\n", .{chip8.registers[idx]});
+            }
 
             std.debug.print("i: {x}\n", .{i.*});
 
-            std.debug.print("ms nib: {x}\n", .{(opcode_uint & 0xf000) >> 12});
             std.debug.print("\n\n", .{});
         }
 
-        switch ((opcode_uint & 0xf000) >> 12) {
+        switch ((opcode & 0xf000) >> 12) {
             0x0 => {
-                if (opcode_uint == 0xe0) {
-                    // 0x00e0: clear the display
+                if (opcode == 0xe0) {
+                    // 0x00E0: clear the display
                     @memset(display, 0);
                     pc.* += 2;
-                } else if (opcode_uint == 0xee) {
-                    // 0x00ee: return from subroutine
+                } else if (opcode == 0xee) {
+                    // 0x00EE: return from subroutine
                     chip8.stack_index -= 1;
                     pc.* = chip8.address_stack[chip8.stack_index];
                 } else {
@@ -131,60 +183,60 @@ const Chip8 = struct {
                 pc.* += 2;
             },
             0x7 => {
-                // add NN to VX (carry flag is not changed)
+                // 0x7XNN: add NN to VX (carry flag is not changed)
                 vx.* +%= nn;
                 pc.* += 2;
             },
             0x8 => {
                 switch (n) {
                     0x0 => {
-                        // set VX to VY
+                        // 0x8XY0: set VX to VY
                         vx.* = vy.*;
                     },
                     0x1 => {
-                        // set VX to VX bit-or VY
+                        // 0x8XY1: set VX to VX bit-or VY
                         vx.* |= vy.*;
                     },
                     0x2 => {
-                        // set VX to VX bit-and VY
+                        // 0x8XY2: set VX to VX bit-and VY
                         vx.* &= vy.*;
                     },
                     0x3 => {
-                        // set VX to VX bit-xor VY
+                        // 0x8XY3: set VX to VX bit-xor VY
                         vx.* ^= vy.*;
                     },
                     0x4 => {
-                        // add VY to VX (carry flag changed)
+                        // 0x8XY4: add VY to VX (carry flag changed)
                         const res = vx.* +% vy.*;
                         vf.* = @intFromBool(res < vx.*);
                         vx.* = res;
                     },
                     0x5 => {
-                        // sub VY from VX (carry flag changed)
+                        // 0x8XY5: sub VY from VX (carry flag changed)
                         vf.* = @intFromBool((vx.* >= vy.*));
                         vx.* -%= vy.*;
                     },
                     0x6 => {
-                        // shift VX to the right (carry flag set to lsb)
+                        // 0x8XY6: shift VX to the right (carry flag set to lsb)
                         vf.* = vx.* & 0x01;
                         vx.* >>= 1;
                     },
                     0x7 => {
-                        // subtract VX from VY, store in VX (carry flag changed)
+                        // 0x8XY7: subtract VX from VY, store in VX (carry flag changed)
                         vf.* = @intFromBool((vy.* >= vx.*));
                         vx.* = vy.* -% vx.*;
                     },
                     0xe => {
-                        // shift VX to the right (carry flag set to lsb)
+                        // 0x8XYE: shift VX to the right (carry flag set to lsb)
                         vf.* = vx.* & 0x80;
                         vx.* <<= 1;
                     },
-                    else => unknown(opcode_uint),
+                    else => unknown(opcode),
                 }
                 pc.* += 2;
             },
             0x9 => {
-                // skip the next instruction if VX != VY
+                // 0x9XY0: skip the next instruction if VX != VY
                 pc.* += if (vx.* != vy.*) 4 else 2;
             },
             0xa => {
@@ -193,23 +245,18 @@ const Chip8 = struct {
                 pc.* += 2;
             },
             0xb => {
-                // jump to address nnn + V0
+                // 0xBNNN: jump to address nnn + V0
                 pc.* = nnn + chip8.registers[0x0];
                 pc.* += 2;
             },
             0xc => {
-                // VX = rand (0..255) & NN
+                // 0xCXNN: VX = rand (0..255) & NN
                 vx.* = random.uintAtMost(u8, 255) & nn;
                 pc.* += 2;
             },
             0xd => {
-                // 0xDXYN:
-                // Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels
-                // and a height of N pixels. Each row of 8 pixels is read as bit-coded
-                // starting from memory location I; I value does not change after the
-                // execution of this instruction. As described above, VF is set to 1 if
-                // any screen pixels are flipped from set to unset when the sprite is
-                // drawn, and to 0 if that does not happen
+                // 0xDXYN: draw sprite into display memory at pixel coordinate VX VY
+                // using sprite data starting at I, up to I+N
                 vf.* = 0;
                 const sprite_data = chip8.memory[i.* .. i.* + n];
                 var display_index: usize = (vx.* / 8) + (vy.* * 8);
@@ -252,47 +299,43 @@ const Chip8 = struct {
             },
             0xe => {
                 if (nn == 0x9e) {
-                    // if key pressed == key code stored in VX skip next instruction
+                    // 0xEX9E: if key pressed == key code stored in VX skip next instruction
                     pc.* += if (isKeyPressed(vx.*)) 4 else 2;
                 } else if (nn == 0xa1) {
-                    // if key pressed != key code stored in VX skip next instruction
+                    // 0xEXA1: if key pressed != key code stored in VX skip next instruction
                     pc.* += if (!isKeyPressed(vx.*)) 4 else 2;
                 } else {
-                    unknown(opcode_uint);
+                    unknown(opcode);
                 }
             },
             0xf => {
                 switch (nn) {
                     0x07 => {
-                        // store the timer value in VX
+                        // 0xFX07: store the timer value in VX
                         vx.* = chip8.delay_timer;
                     },
                     0x0a => {
-                        // wait for the next key press, and store it in VX
+                        // 0xFX0A: wait for the next key press, and store it in VX
                         vx.* = waitNextKeyPress();
                     },
                     0x15 => {
-                        // set the delay timer to VX
+                        // 0xFX15: set the delay timer to VX
                         chip8.delay_timer = vx.*;
                     },
                     0x18 => {
-                        // set the sound timer to VX
+                        // 0xFX18: set the sound timer to VX
                         chip8.sound_timer = vx.*;
                     },
                     0x1e => {
-                        // add VX to I
+                        // 0xFX1E: add VX to I
                         i.* += vx.*;
                     },
                     0x29 => {
-                        // set I to the character address for VX
+                        // 0xFX29: set I to the character address for VX
                         i.* = 0x50 + ((vx.* & 0x0f) * 4);
                     },
                     0x33 => {
-
-                        // Stores the binary-coded decimal representation of VX,
-                        // with the hundreds digit in memory at location in I,
-                        // the tens digit at location I+1, and the ones digit at
-                        // location I+2
+                        // 0xFX33: store three digit decimal of VX to I, I+1, I+2
                         var num = vx.*;
                         for (0..3) |count| {
                             chip8.memory[i.* + (2 - count)] = num % 10;
@@ -300,42 +343,36 @@ const Chip8 = struct {
                         }
                     },
                     0x55 => {
-                        // Stores from V0 to VX (including VX) in memory,
-                        // starting at address I. The offset from I is increased
-                        // by 1 for each value written,
-                        // but I itself is left unmodified
+                        // 0xFX55: store registers up to VX into memory, from I
 
                         for (0..x + 1) |index| {
                             chip8.memory[i.* + index] = chip8.registers[index];
                         }
                     },
                     0x65 => {
-                        // Fills from V0 to VX (including VX) with values from
-                        // memory, starting at address I. The offset from I is
-                        // increased by 1 for each value read, but
-                        // I itself is left unmodified
+                        // 0xFX65: store memory up to I+X into registers V0 to VX
                         for (0..x + 1) |index| {
                             chip8.registers[index] = chip8.memory[i.* + index];
                         }
                     },
-                    else => unknown(opcode_uint),
+                    else => unknown(opcode),
                 }
                 pc.* += 2;
             },
-            else => unknown(opcode_uint),
+            else => unknown(opcode),
         }
     }
+
+    fn unimpl(op: u16) void {
+        std.debug.print("op 0x{x:04} not implemented\n", .{op});
+        @panic(":)");
+    }
+
+    fn unknown(op: u16) void {
+        std.debug.print("op 0x{x:04} is not valid\n", .{op});
+        @panic(":(");
+    }
 };
-
-fn unimpl(op: u16) void {
-    std.debug.print("op 0x{x:04} not implemented\n", .{op});
-    @panic(":)");
-}
-
-fn unknown(op: u16) void {
-    std.debug.print("op 0x{x:04} is not valid\n", .{op});
-    @panic(":(");
-}
 
 // TODO: implement
 fn isKeyPressed(key_code: u8) bool {
@@ -349,6 +386,8 @@ fn waitNextKeyPress() u8 {
 }
 
 pub fn main(init: std.process.Init) !void {
+    defer _ = init.arena.reset(.free_all);
+
     var chip8: Chip8 = .{};
     var rng = std.Random.DefaultPrng.init(
         @bitCast(std.Io.Timestamp.now(
@@ -358,11 +397,19 @@ pub fn main(init: std.process.Init) !void {
     );
     random = rng.random();
 
-    const rom = try Rom.load(init.arena.allocator(), init.io, "roms/particle_demo_zeroZshadow_2008.ch8");
+    const args = try init.minimal.args.toSlice(init.arena.allocator());
+    if (args.len != 2) {
+        std.debug.print("chip8z rom_file_path\n", .{});
+        return;
+    }
+
+    const rom: Rom = try .load(init.arena.allocator(), init.io, args[1]);
+    defer rom.unload(init.arena.allocator());
+
     chip8.loadRom(&rom);
 
-    var buf: [2046]u8 = @splat(0);
-    var stdout = std.Io.File.stdout().writer(init.io, &buf);
+    var stdout_buf: [1024 * 3]u8 = @splat(0);
+    var stdout = std.Io.File.stdout().writer(init.io, &stdout_buf);
 
     var display_cache: [0x100]u8 = @splat(0);
 
@@ -370,16 +417,18 @@ pub fn main(init: std.process.Init) !void {
     var timers_timestamp = std.Io.Timestamp.now(init.io, .awake);
 
     while (true) {
-        defer stdout.interface.flush() catch {};
+        defer if (cli) stdout.interface.flush() catch {};
+        if (cli) {
+            try stdout.interface.print("\x1b[H", .{});
+            try stdout.interface.flush();
+        }
 
-        try stdout.interface.print("\x1b[H", .{});
-        try stdout.interface.flush();
         const render_delta_time = render_timestamp.untilNow(init.io, .awake).nanoseconds;
         const timers_delta_time = timers_timestamp.untilNow(init.io, .awake).nanoseconds;
 
         // 60hz: decrement timers > 0
-        if (timers_delta_time < 16_000_000) {
-            try stdout.interface.print("timers dt: {d}\n", .{render_delta_time});
+        if (timers_delta_time < 16_666_667) {
+            if (cli) try stdout.interface.print("timers dt: {d}\n", .{render_delta_time});
         } else {
             timers_timestamp = std.Io.Timestamp.now(init.io, .awake);
             chip8.delay_timer -|= 1;
@@ -388,7 +437,7 @@ pub fn main(init: std.process.Init) !void {
 
         // 1000hz: step cpu
         if (render_delta_time < 1_000_000) {
-            try stdout.interface.print("render dt: {d}\n", .{render_delta_time});
+            if (cli) try stdout.interface.print("render dt: {d}\n", .{render_delta_time});
             continue;
         } else {
             render_timestamp = std.Io.Timestamp.now(init.io, .awake);
@@ -396,23 +445,29 @@ pub fn main(init: std.process.Init) !void {
 
         chip8.step();
 
+        // only re-render if the display has changed
+        // smart price retained-ui
         if (!std.mem.eql(u8, &display_cache, chip8.memory[0xf00..])) {
             @memcpy(&display_cache, chip8.memory[0xf00..]);
-            try stdout.interface.print("\n", .{});
-            for (display_cache, 0..) |byte, i| {
-                if (i % 8 == 0) {
-                    try stdout.interface.print("\n", .{});
+
+            if (cli) {
+                try stdout.interface.print("\n", .{});
+                for (display_cache, 0..) |byte, i| {
+                    if (i % 8 == 0) {
+                        try stdout.interface.print("\n", .{});
+                    }
+                    for (0..8) |bit| {
+                        const shift: u3 = @intCast(7 - bit);
+                        try stdout.interface.print("{s}", .{if ((byte >> shift) & 0x01 == 1) "X " else ". "});
+                    }
                 }
-                for (0..8) |bit| {
-                    const shift: u3 = @intCast(7 - bit);
-                    try stdout.interface.print("{s}", .{if ((byte >> shift) & 0x01 == 1) "X " else ". "});
-                }
+                try stdout.interface.print("\n", .{});
             }
-            try stdout.interface.print("\n", .{});
         }
     }
 
-    try stdout.interface.print("\n", .{});
-    try stdout.interface.flush();
-    _ = init.arena.reset(.free_all);
+    if (cli) {
+        try stdout.interface.print("\n", .{});
+        try stdout.interface.flush();
+    }
 }
