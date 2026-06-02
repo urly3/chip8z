17088dc007a8a9f6160695cca084c277c4f7f5d2
urly3 <u@rl.yyy>
2026-06-02 13:51:03 +0100

    build.zig: lib name

--- build.zig
+++ build.zig
@@ -14,7 +14,7 @@ pub fn build(b: *std.Build) void {
     });
     exe.root_module.addLibraryPath(b.path("./lib/sdl"));
     exe.root_module.linkSystemLibrary(
-        "SDL3.dll",
+        "SDL3",
         .{
             .needed = true,
             .preferred_link_mode = .static,
